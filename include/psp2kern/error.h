8bb1ff947cb96e8798f803213029f673ab50f34e
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-06-19 12:00:10 -0400

    Add error headers

---
+++ include/psp2kern/error.h
@@ -0,0 +1,11 @@
+/**
+ * \kernelgroup{SceError}
+ * \usage{psp2kern/error.h}
+ */
+
+#ifndef _DOLCESDK_PSP2KERN_ERROR_H_
+#define _DOLCESDK_PSP2KERN_ERROR_H_
+
+#include <psp2common/error.h>
+
+#endif // _DOLCESDK_PSP2KERN_ERROR_H_
---
+++ include/psp2kern/kernel/error.h
@@ -0,0 +1,11 @@
+/**
+ * \kernelgroup{SceKernelError}
+ * \usage{psp2kern/kernel/error.h}
+ */
+
+#ifndef _DOLCESDK_PSP2KERN_KERNEL_ERROR_H_
+#define _DOLCESDK_PSP2KERN_KERNEL_ERROR_H_
+
+#include <psp2common/kernel/error.h>
+
+#endif // _DOLCESDK_PSP2KERN_KERNEL_ERROR_H_
