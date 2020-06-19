8dae8a16b73d1b04dc4cf4bd4430d860a99d2eed
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-06-19 00:41:35 -0400

    Add arm.h

---
+++ include/psp2common/arm.h
@@ -0,0 +1,17 @@
+/**
+ * \usergroup{SceCPU}
+ * \usage{psp2common/arm.h}
+ */
+
+#ifndef _DOLCESDK_PSP2COMMON_ARM_H_
+#define _DOLCESDK_PSP2COMMON_ARM_H_
+
+#define SCE_ARM_L1_DCACHE_LINE_SIZE		32
+#define SCE_ARM_L1_ICACHE_LINE_SIZE		32
+
+#define SCE_ARM_L1_WAYS					4
+
+#define SCE_ARM_L2_CACHE_LINE_SIZE		32
+#define SCE_ARM_L2_CACHE_MAX_FILL_SIZE	64
+
+#endif // _DOLCESDK_PSP2COMMON_ARM_H_
