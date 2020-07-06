c12c30dd54f616b5fe5561a10ee386930fcc5d26
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-07-05 20:51:14 -0400

    Update aggregate headers
    This is to check multiple definitions within the sdk.

--- include/dolcesdkkern.h
+++ include/dolcesdkkern.h
@@ -3,44 +3,48 @@
 
 #include <dolcesdk/utils.h>
 
-#include <psp2kern/types.h>
 #include <psp2kern/appmgr.h>
+#include <psp2kern/arm.h>
+#include <psp2kern/audioout.h>
+#include <psp2kern/avcodec/jpegenc.h>
 #include <psp2kern/bt.h>
 #include <psp2kern/ctrl.h>
 #include <psp2kern/display.h>
-#include <psp2kern/power.h>
-#include <psp2kern/registrymgr.h>
-#include <psp2kern/sblacmgr.h>
-#include <psp2kern/sblaimgr.h>
-#include <psp2kern/sblauthmgr.h>
-#include <psp2kern/syscon.h>
-#include <psp2kern/uart.h>
-#include <psp2kern/udcd.h>
-#include <psp2kern/usbd.h>
-#include <psp2kern/usbserial.h>
-
-#include <psp2kern/avcodec/jpegenc.h>
-
+#include <psp2kern/error.h>
+#include <psp2kern/idstorage.h>
 #include <psp2kern/io/devctl.h>
 #include <psp2kern/io/dirent.h>
 #include <psp2kern/io/fcntl.h>
 #include <psp2kern/io/stat.h>
-
+#include <psp2kern/kernel/constants.h>
 #include <psp2kern/kernel/cpu.h>
 #include <psp2kern/kernel/dmac.h>
+#include <psp2kern/kernel/error.h>
+#include <psp2kern/kernel/excpmgr.h>
 #include <psp2kern/kernel/intrmgr.h>
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/kernel/processmgr.h>
 #include <psp2kern/kernel/suspend.h>
 #include <psp2kern/kernel/sysmem.h>
 #include <psp2kern/kernel/threadmgr.h>
-
+#include <psp2kern/kernel/types.h>
+#include <psp2kern/kernel/utils.h>
 #include <psp2kern/lowio/dsi.h>
 #include <psp2kern/lowio/gpio.h>
 #include <psp2kern/lowio/i2c.h>
 #include <psp2kern/lowio/iftu.h>
 #include <psp2kern/lowio/pervasive.h>
-
 #include <psp2kern/net/net.h>
+#include <psp2kern/power.h>
+#include <psp2kern/registrymgr.h>
+#include <psp2kern/sblacmgr.h>
+#include <psp2kern/sblaimgr.h>
+#include <psp2kern/sblauthmgr.h>
+#include <psp2kern/syscon.h>
+#include <psp2kern/types.h>
+#include <psp2kern/uart.h>
+#include <psp2kern/udcd.h>
+#include <psp2kern/usbd.h>
+#include <psp2kern/usbserial.h>
 
 #endif
