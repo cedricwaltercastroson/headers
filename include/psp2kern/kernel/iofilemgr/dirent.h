06cb2746b50f6218ba2ef13b3ad7992eb816b252
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-07-23 13:48:34 -0400

    Refactor kernel IO functions

---
+++ include/psp2kern/kernel/iofilemgr/dirent.h
@@ -0,0 +1,63 @@
+/**
+ * \kernelgroup{SceIofilemgr}
+ * \usage{psp2kern/kernel/iofilemgr/dirent.h,SceIofilemgrForDriver_stub}
+ */
+
+#ifndef _DOLCESDK_PSP2KERN_KERNEL_IOFILEMGR_DIRENT_H_
+#define _DOLCESDK_PSP2KERN_KERNEL_IOFILEMGR_DIRENT_H_
+
+#include <psp2kern/kernel/iofilemgr/stat.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Describes a single directory entry */
+typedef struct SceIoDirent {
+	SceIoStat d_stat;      //!< File status
+	char      d_name[256]; //!< File name (not file path)
+	void      *d_private;  //!< Reserved (set to 0)
+	int       dummy;       //!< Padding
+} SceIoDirent;
+
+/**
+  * Open a directory
+  *
+  * @par Example:
+  * @code
+  * int dfd;
+  * dfd = ksceIoDopen("device:/");
+  * if(dfd >= 0)
+  * { Do something with the file descriptor }
+  * @endcode
+  * @param dirname - The directory to open for reading.
+  * @return If >= 0 then a valid file descriptor, otherwise a Sony error code.
+  */
+SceUID ksceIoDopen(const char *dirname);
+
+/**
+  * Close an opened directory file descriptor
+  *
+  * @param fd - Already opened file descriptor (using ::ksceIoDopen)
+  * @return < 0 on error
+  */
+int ksceIoDclose(SceUID fd);
+
+/**
+  * Reads an entry from an opened file descriptor.
+  *
+  * @param fd - Already opened file descriptor (using ::ksceIoDopen)
+  * @param buf - Pointer to a ::SceIoDirent structure to hold the file information
+  *
+  * @return Read status
+  * -   0 - No more directory entries left
+  * - > 0 - More directory entries to go
+  * - < 0 - Error
+  */
+int ksceIoDread(SceUID fd, SceIoDirent *buf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _DOLCESDK_PSP2KERN_KERNEL_IOFILEMGR_DIRENT_H_ */
