de2db00a90466d14c73ada6684a02bd7be525d88
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-08-28 20:18:14 -0400

    travis-ci: install doxygen from distro repo

--- .travis.d/deploy_docs.sh
+++ .travis.d/deploy_docs.sh
@@ -2,10 +2,10 @@
 set -e
 
 cd $TRAVIS_BUILD_DIR/docs
-$HOME/doxygen/doxygen*/bin/doxygen
-cp search.js html/search/search.js  
-echo "<script id=\"searchdata\" type=\"text/xmldata\">" >> html/search.html  
-cat searchdata.xml >> html/search.html  
+doxygen
+cp search.js html/search/search.js
+echo "<script id=\"searchdata\" type=\"text/xmldata\">" >> html/search.html
+cat searchdata.xml >> html/search.html
 echo "</script>" >> html/search.html
 
 cd $TRAVIS_BUILD_DIR
