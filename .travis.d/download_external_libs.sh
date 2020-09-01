81434fb040a1cd56618a8128aed94619dae03a90
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-09-01 17:23:09 -0400

    travis-ci: update script

--- .travis.d/download_external_libs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-curl -sL https://github.com/DolceSDK/ddpm/raw/master/ddpm -o ddpm
-chmod +x ddpm
-curl -L https://github.com/DolceSDK/ddpm/raw/master/include/install-packages.sh -o install-packages.sh
-sed -i "s/\.\.\///" install-packages.sh
-curl -L https://github.com/DolceSDK/ddpm/raw/master/install-all.sh | sed -e "s/include\///" | bash > /dev/null
-rm ddpm
