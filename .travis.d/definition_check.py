b0e4068a9c1758119dd1a11c4626f63a5a3190bb
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-09-22 21:16:03 -0400

    Disable doc generation and group check

--- .travis.d/definition_check.py
+++ .travis.d/definition_check.py
@@ -4,14 +4,9 @@ import re
 import fnmatch
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-DEF_FILE = 'definitions.dox'
-DEF_FILE_PATH = os.path.join(CURR_DIR, '..', 'docs', DEF_FILE)
 DB_FILE = 'db.yml'
 DB_FILE_PATH = os.path.join(CURR_DIR, '..', 'db.yml')
 INCLUDE_DIR = os.path.join(CURR_DIR, '..', 'include')
-
-DEFINE_RULE = re.compile(r' \*     \\defgroup (Sce\w+) \w+')
-USER_GROUP_RULE = re.compile(r' \* \\(user|kernel)group\{(Sce\w+)\}')
 
 FUNC_RULE_PATTERN = (
     # ret
@@ -49,16 +44,6 @@ def dict_items(dic):
         return dic.iteritems()
     return dic.items()
 
-def read_def_groups():
-    definitions = dict()
-    with open(DEF_FILE_PATH, 'r') as d:
-        for line in readlines(d):
-            m = DEFINE_RULE.match(line)
-            if not m:
-                continue
-            definitions[m.group(1)] = 0
-    return definitions
-
 def read_nids():
     errors = []
     user_nids = dict()
@@ -83,38 +68,6 @@ def read_nids():
                 continue
             nids[k] = 1
     return dict(user_nids, **kernel_nids), errors
-
-def check_header_groups(definitions):
-    errors = []
-    # check exists in definitions
-    for header_path in findfile(INCLUDE_DIR, '*.h'):
-        header_file = header_path.split('include/')[1]
-        if header_file in IGNORE_FILES:
-            continue
-        with open(header_path, 'r') as h:
-            have_group_define = False
-            for line in readlines(h):
-                m = USER_GROUP_RULE.match(line)
-                if not m:
-                    continue
-                if have_group_define:
-                    errors.append('%s: Has multiple groups' % header_file)
-                    break
-                group = m.group(2)
-                if definitions.get(group) == None:
-                    errors.append('%s: Unknown group %s' %
-                                  (header_file, m.group(2)))
-                    break
-                definitions[group] += 1
-                have_group_define = True
-            if not have_group_define:
-                errors.append('%s: Could not find definition' % header_file)
-    # reverse check if exist header
-    for k, v in dict_items(definitions):
-        if v == 0:
-            errors.append('%s: Could not find using header: %s' %
-                          (DEF_FILE, k))
-    return errors
 
 def check_function_nids(nids):
     errors = []
@@ -142,8 +95,7 @@ def check_function_nids(nids):
 
 if __name__ == '__main__':
     nids, errors = read_nids()
-    errors += check_header_groups(read_def_groups()) \
-        + check_function_nids(nids)
+    errors += check_function_nids(nids)
     if len(errors):
         for e in errors:
             print(e)
