2a0372b4994310e902d685faaa75e336c2525454
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-09-22 21:59:41 -0400

    Remove deprecated headers

--- include/psp2/shellutil.h
+++
@@ -1,8 +0,0 @@
-/**
- * \usergroup{SceShellSvc}
- * \usage{psp2/shellsvc.h,SceShellSvc_stub}
- */
-
-#pragma message "psp2/shellutil.h is deprecated. Please include psp2/shellsvc.h."
-
-#include <psp2/shellsvc.h>
